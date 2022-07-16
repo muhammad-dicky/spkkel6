46dbbc132c469595659e6f315c66cd97b46dc9ca
muhammad-dicky <muhammad.1330@students.amikom.ac.id>
2022-07-16 23:12:22 +0700

    first

---
+++ application/views/backend/dashboard.php
@@ -0,0 +1,66 @@
+		    <div class="row">
+	        	<div class="col-sm-3">
+	        	
+	        		<div class="tile-stats tile-red">
+	        			<div class="icon"><i class="entypo-users"></i></div>
+	        			<div class="num" data-start="0" data-end=" <?php echo count($jumlah_users); ?> " data-postfix="" data-duration="1500" data-delay="0">0</div>
+	        			
+	        			<h3>Jumlah Users</h3>
+	        			<p> yang ada pada Website.</p>
+	        		</div>
+	        		
+	        	</div>
+	        	
+	        	<div class="col-sm-3">
+	        	
+	        		<div class="tile-stats tile-green">
+	        			<div class="icon"><i class="entypo-chart-bar"></i></div>
+	        			<div class="num" data-start="0" data-end="<?php echo count($jumlah_kriteria); ?>" data-postfix="" data-duration="1500" data-delay="600">0</div>
+	        			
+	        			<h3>Jumlah Kriteria</h3>
+	        			<p> yang ada pada Website.</p>
+	        		</div>
+	        		
+	        	</div>
+	        	
+	        	<div class="col-sm-3">
+	        	
+	        		<div class="tile-stats tile-aqua">
+	        			<div class="icon"><i class="entypo-suitcase"></i></div>
+	        			<div class="num" data-start="0" data-end=" <?php echo count($jumlah_sekolah) ?> " data-postfix="" data-duration="1500" data-delay="1200">0</div>
+	        			
+	        			<h3>Jumlah Sekolah</h3>
+	        			<p>Sekolah yang ada di kota makassar.</p>
+	        		</div>
+	        		
+	        	</div>
+	        	
+	        	<div class="col-sm-3">
+	        	
+	        		<div class="tile-stats tile-primary">
+	        			<div class="icon"><i class="entypo-rss"></i></div>
+	        			<div class="num" data-start="0" data-end="52" data-postfix="" data-duration="1500" data-delay="1800">0</div>
+	        			
+	        			<h3>Jumlah Pengunjung</h3>
+	        			<p>on our site right now.</p>
+	        		</div>
+	        		
+	        	</div>
+	        </div>
+	        <br />
+
+	        <div class="row">
+	        	<div class="col-md-6">
+		
+					<div class="panel panel-info" data-collapsed="0">
+						
+						<!-- panel head -->
+						<div class="panel-heading">
+							
+						
+					</div>
+		
+				</div>
+	        </div>
+
+	       
